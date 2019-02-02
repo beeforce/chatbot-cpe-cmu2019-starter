e6a2e185008061ae449ede7d4088639647e43314
Buri Paoton <bakeforce75@gmail.com>
2019-02-02 12:43:52 +0700

    console.log

--- server.js
+++ server.js
@@ -19,12 +19,11 @@ app.post('/webhook', middleware(config), (req, res) => {
 
   if (event.type === 'message') {
     const message = event.message;
-
+    console.log(message)
     client.replyMessage(event.replyToken, {
       type: 'text',
       text: message.text // change from message to message.text
     });
-
   }
 })
 
