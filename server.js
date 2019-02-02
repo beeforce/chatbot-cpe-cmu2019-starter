4f529406eab3233639f050d3111064b149a65a9e
Buri Paoton <bakeforce75@gmail.com>
2019-02-02 13:52:07 +0700

    edit text response type

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.post('/webhook', middleware(config), (req, res) => {
     console.log(message)
     client.replyMessage(event.replyToken, {
       type: 'text',
-      text: message.text // change from message to message.text
+      text: message.type // change from message to message.text
     });
   }
 })
